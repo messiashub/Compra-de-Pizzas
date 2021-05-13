b73f19b29e92f5486dda3d6b672e519d17b97acd
messiashub <claudineimessias@hotmail.com>
2021-05-13 00:05:03 -0300

    update

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ console.log(pizzaJson);
 const c = (elemento)=> document.querySelector(elemento);  // Vai retornar o item
 const cs = (elemento)=> document.querySelectorAll(elemento); // Vai retornar um array com os itens
 
+
+// 1
 pizzaJson.map((pizza,index)=>{ // Para cada objeto dentro de pizzaJson mapeie e adciona a classe ".pizza-item"
     let pizzaItem = c('.models .pizza-item').cloneNode(true); // Clonando a classe e jogando dentro da classe ".pizza-area"
     // preencher as informações em pizza-item
@@ -14,6 +16,19 @@ pizzaJson.map((pizza,index)=>{ // Para cada objeto dentro de pizzaJson mapeie e 
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `RS:${pizza.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
+    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('clicou');
+// 2 MODAL
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display ='flex';
+
+       setTimeout(()=>{
+           c('.pizzaWindowArea').style.opacity =1
+       },200)
+        
+
+    })
 
 
     c('.pizza-area').append(pizzaItem); // pizza-area recebe a classe clonada
